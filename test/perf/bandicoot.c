63c09b11bbab0e76721fb06fb7fb4c9099854ed5
Julius Chrobak <julo@bandilab.org>
2013-01-16 14:16:28 +0100

    fetching ids and using Item bandicoot function to test

--- test/perf/bandicoot.c
+++ test/perf/bandicoot.c
@@ -20,6 +20,7 @@ limitations under the License.
 
 #define CONNS 16
 #define ITERATIONS 1024
+#define IDS_CNT 100000
 
 int THREADS;
 
@@ -28,17 +29,21 @@ typedef struct {
     int len;
 } Exec;
 
-static void get(IO *io, char *fn)
+static int get(IO *io, char *fn)
 {
     Http_Args args = {.len = 0};
     if (http_get(io, fn, &args) != 200)
         fail();
 
     Http_Resp *resp = http_parse_resp(io);
-    if (resp == NULL || resp->status != 200)
-        fail();
+    int res = 0;
 
-    http_free_resp(resp);
+    if (resp != NULL) {
+        res = resp->status;
+        http_free_resp(resp);
+    }
+
+    return res;
 }
 
 static IO *conn(IO *ios[CONNS], int i, char *addr)
@@ -55,6 +60,36 @@ static void close(IO *io) {
         sys_close(io);
 }
 
+static int *get_ids(const char *addr, int *len) {
+    IO *io = sys_connect(addr, IO_STREAM);
+
+    Http_Args args = {.len = 0};
+    if (http_get(io, "/ids", &args) != 200)
+        fail();
+
+    Http_Resp *resp = http_parse_resp(io);
+    if (resp == NULL || resp->status != 200)
+        fail();
+    
+    int cnt = 0;
+    char **ids = str_split_big(resp->body, "\n", &cnt);
+
+    int *res = mem_alloc(cnt * sizeof(int));
+
+    for (int i = 0; i < cnt; ++i) {
+        int err = 0;
+        int val = str_int(ids[i], &err);
+        if (!err)
+            res[(*len)++] = val;
+    }
+
+    mem_free(ids);
+    http_free_resp(resp);
+    sys_close(io);
+
+    return res;
+}
+
 static void *exec_thread(void *arg)
 {
     Exec *e = arg;
@@ -66,10 +101,13 @@ static void *exec_thread(void *arg)
         last[i] = 0;
     }
 
+    int cnt = 1;
+    int *ids = get_ids(e->addrs[0], &cnt);
+
     for (int i = 0; i < ITERATIONS; ++i) {
         long long time = sys_millis();
 
-        int closed = 0, idx = rand() % e->len;
+        int closed = 0, idx = rand() % e->len, id = rand() % cnt;
         IO *io = ios[idx];
 
         if (time - last[idx] > 4000) {
@@ -78,18 +116,23 @@ static void *exec_thread(void *arg)
             closed = 1;
         }
 
-        get(io, "/cnt");
+        char fn[MAX_NAME];
+        str_print(fn, "/Item?id=%d", ids[id]);
+        int res = get(io, fn);
         last[idx] = sys_millis();
 
-        sys_print("%lld,%lld,%s,%d\n",
+        sys_print("%lld,%lld,%s,%d,%d\n",
                   last[idx] / 1000,
                   last[idx] - time,
                   e->addrs[idx],
-                  closed);
+                  closed,
+                  res);
     }
 
     for (int i = 0; i < e->len; ++i)
         close(ios[i]);
+
+    mem_free(ids);
 
     return NULL;
 }
